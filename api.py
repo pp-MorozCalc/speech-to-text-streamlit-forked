b36f26c5547f35c1b10d118fda68d56889b215ee
Vycheslav Russu <vycheslavrussu@gmail.com>
2024-05-19 23:58:57 +0500

    Updates: 1. Code style in api.py has been fixed 2. Code style in main.py has been fixed 3. Names in main have been fixed

--- api.py
+++ api.py
@@ -7,39 +7,43 @@ import whisper
 app = FastAPI()
 
 # Определяем директорию для хранения загруженных файлов
-upload_files_directory = 'uploads/'
+upload_files_directory = "uploads/"
+
 
 # Создаем маршрут для загрузки файла
 @app.post("/uploadfile/")
 async def create_upload_file(request: Request, file: UploadFile = File(...)):
-  try:
-      # Открываем файл в режиме записи байтов
-      with open(os.path.join(upload_files_directory, file.filename), 'wb') as buffer:
-          # Читаем содержимое файла и записываем его в буфер
-          contents = await file.read()
-          buffer.write(contents)
-      # Возвращаем имя загруженного файла
-      return {"filename": file.filename}
-  except Exception as e:
-      # Возвращаем сообщение об ошибке в случае исключения
-      return {"error": str(e)}
+    try:
+        # Открываем файл в режиме записи байтов
+        with open(os.path.join(upload_files_directory, file.filename), "wb") as buffer:
+            # Читаем содержимое файла и записываем его в буфер
+            contents = await file.read()
+            buffer.write(contents)
+        # Возвращаем имя загруженного файла
+        return {"filename": file.filename}
+    except Exception as e:
+        # Возвращаем сообщение об ошибке в случае исключения
+        return {"error": str(e)}
+
 
 # Создаем маршрут для транскрибирования аудио
 @app.get("/transcribe/{filename}")
 async def transcribe_audio(filename: str):
-  # Загружаем модель whisper
-  model = whisper.load_model('base')
-  # Транскрибируем аудиофайл
-  result = model.transcribe(os.path.join(upload_files_directory, filename))
-  # Возвращаем результат транскрибирования
-  return {"text": result['text']}
+    # Загружаем модель whisper
+    model = whisper.load_model("base")
+    # Транскрибируем аудиофайл
+    result = model.transcribe(os.path.join(upload_files_directory, filename))
+    # Возвращаем результат транскрибирования
+    return {"text": result["text"]}
+
 
 # Эта функция возвращает список всех загруженных файлов.
 # Она просто читает все файлы в директории upload_files_directory и возвращает их имена.
 @app.get("/listfiles")
 async def list_files():
-   files = os.listdir(upload_files_directory)
-   return {"files": files}
+    files = os.listdir(upload_files_directory)
+    return {"files": files}
+
 
 # Эта функция удаляет указанный файл.
 # Она принимает имя файла в качестве параметра, проверяет, существует ли такой файл,
@@ -47,9 +51,9 @@ async def list_files():
 # Если файла не существует, возвращается сообщение об ошибке.
 @app.delete("/deletefile/{filename}")
 async def delete_file(filename: str):
-   file_path = os.path.join(upload_files_directory, filename)
-   if os.path.exists(file_path):
-       os.remove(file_path)
-       return {"message": f"{filename} deleted successfully"}
-   else:
-       return {"error": f"{filename} does not exist"}
+    file_path = os.path.join(upload_files_directory, filename)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return {"message": f"{filename} deleted successfully"}
+    else:
+        return {"error": f"{filename} does not exist"}
--- main.py
+++ main.py
@@ -4,48 +4,67 @@ import streamlit as st
 
 # Set page config
 st.set_page_config(
-    page_title='AI Speech to Text converter',
-    page_icon='🎙️',
-    layout='centered',
-    initial_sidebar_state='auto',
+    page_title="AI Speech to Text converter",
+    page_icon="🎙️",
+    layout="centered",
+    initial_sidebar_state="auto",
 )
 
 # UI
-st.title('Расшифруйте аудио или видео в текст ✨')
-st.info('Загрузите ваш файл и получите его текстовую расшифровку. Сервис поддерживает все популярные аудио и видео форматы.')
+st.title("Расшифруйте аудио или видео в текст ✨")
+st.info(
+    "Загрузите ваш файл и получите его текстовую расшифровку. Сервис поддерживает все популярные аудио и видео форматы."
+)
 st.divider()
 
-upload_files_directory = 'uploads/'
+upload_files_directory = "uploads/"
+
 
 # Upload audio or video file
-def Upload_audio_file():
+def upload_audio_file():
     # Upload file
     uploaded_object = st.file_uploader(
-        type=['opus', 'mp3', 'aac', 'flac', 'wv', 'wav',
-              'mp4', 'mov', 'wmv', 'webm', 'avi', 'mkv'],
-        label='Выберите аудио или видео для распознавания',
+        type=[
+            "opus",
+            "mp3",
+            "aac",
+            "flac",
+            "wv",
+            "wav",
+            "mp4",
+            "mov",
+            "wmv",
+            "webm",
+            "avi",
+            "mkv",
+        ],
+        label="Выберите аудио или видео для распознавания",
         accept_multiple_files=False,
     )
 
     # Save uploaded bytes as file
     if uploaded_object is not None:
-        with open(os.path.join(upload_files_directory, uploaded_object.name), 'wb') as file:
+        with open(
+            os.path.join(upload_files_directory, uploaded_object.name), "wb"
+        ) as file:
             file.write(uploaded_object.getbuffer())
         return uploaded_object.name
     else:
         return None
 
+
 # Transcribe audio
-def Trasncribe(audio):
-    model = whisper.load_model('base')
+def trasncribe(audio):
+    model = whisper.load_model("base")
     result = model.transcribe(audio)
-    st.write(result['text'])
+    st.write(result["text"])
+
 
 # Getting name of uploading file to put it into model
-audio_file = Upload_audio_file()
+audio_file = upload_audio_file()
 
-# Call Trasncribe function
-action = st.button('Распознать аудио', type='primary')
+# Call trasncribe function
+action = st.button("Распознать аудио", type="primary")
 if action:
     st.subheader(audio_file)
-    st.write(Trasncribe(os.path.join(upload_files_directory, audio_file)))
+    st.write(trasncribe(os.path.join(upload_files_directory, audio_file)))
